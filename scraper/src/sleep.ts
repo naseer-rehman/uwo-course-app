1057c87b9b5535b87e78c449fc0936ba1169845b
Naseer Rehman <rehmn.nas@gmail.com>
2023-07-25 21:25:42 -0400

    clean up main file
    - extracted the academic calendar and timetable scraping logic into
      their own respective files

--- scraper/src/sleep.ts
+++ scraper/src/sleep.ts
@@ -2,7 +2,7 @@
  * @param time - in seconds
  * @returns a promise that resolves in the specified time
  */
-export function sleep(time: number) {
+export default function sleep(time: number) {
   return new Promise(
     resolve => setTimeout(resolve, time * 1000)
   );
